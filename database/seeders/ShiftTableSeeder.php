55c0bab49b0509b7a68332527b0b3af9dec9b18a
Kossy-Augusta <kossyudogu@gmail.com>
2024-06-10 15:46:19 +0100

    seed data to the shift table

---
+++ database/seeders/ShiftTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Shift;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class ShiftTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $shifts =[
+            ['title' => 'Morning', 'start_time'=> '2024-06-10 08:00:00', 'end_time' => '2024-06-10 18:00:00'],
+            ['title' => 'Night', 'start_time'=> '2024-06-10 18:00:00', 'end_time' => '2024-06-10 08:00:00']
+        ];
+
+        foreach ($shifts as $shift)
+        {
+            Shift::create($shift);
+        }
+    }
+}
