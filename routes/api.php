4b6a2f69ec705e5be6cf9f09a769b5404b0a79f0
Kossy-Augusta <kossyudogu@gmail.com>
2024-06-11 14:57:36 +0100

    shift index page endpoint

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,10 @@ use App\Http\Middleware\AuthApiKey;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Public Route
+Route::get('/shifts', [ShiftController::class, 'index']);
 
+// Protected Routes
 Route::middleware([AuthApiKey::class])->group(function (){
     Route::post('/shifts/create', [ShiftController::class, 'store']);
 });
