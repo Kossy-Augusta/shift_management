ee38a212156b139c607e68e2188e0fb6b2e1bd9f
Kossy-Augusta <kossyudogu@gmail.com>
2024-06-11 10:49:15 +0100

    endpoint for shift creation

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\ShiftController;
 use App\Http\Middleware\AuthApiKey;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware([AuthApiKey::class])->group(function (){
-
+    Route::post('/shifts/create', [ShiftController::class, 'store']);
 });
