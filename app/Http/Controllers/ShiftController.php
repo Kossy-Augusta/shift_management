656f33daeaac18101cfd1a377936fd3d990e8f1c
Kossy-Augusta <kossyudogu@gmail.com>
2024-06-11 14:56:56 +0100

    index method to get all the shifts scheduled

--- app/Http/Controllers/ShiftController.php
+++ app/Http/Controllers/ShiftController.php
@@ -5,12 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\Shift;
 use App\Models\ShiftStaff;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Auth\Events\Validated;
 use App\Http\Requests\CreateShiftRequest;
-use Illuminate\Http\Response;
 
 class ShiftController extends Controller
 {
+        //  Show all shifts
+    public function index()
+    {
+        // $products = DB::select('SELECT * FROM shift_staff ORDER BY created_at DESC LIMIT 21');
+        // get the last 20 inputs in descending oorder and paginate
+        $products = ShiftStaff::latest()->take(20)->paginate(5);
+         return response()->json(['error' => false, "data" => $products]);
+    }
     // Create a shift schedule
     public function store(CreateShiftRequest $request)
     {
