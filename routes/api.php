a3fcbc6fec3116bdc396fe2a877141e22fd357d4
Kossy-Augusta <kossyudogu@gmail.com>
2024-06-14 16:32:12 +0100

    added named routes

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Public Route
-Route::get('/shifts', [ShiftController::class, 'index']);
+Route::get('/shifts', [ShiftController::class, 'index'])->name('all_shifts');
 
 // Protected Routes
 Route::middleware([AuthApiKey::class])->group(function (){
-    Route::post('/shifts/create', [ShiftController::class, 'store']);
+    Route::post('/shifts/create', [ShiftController::class, 'store'])->name('create');
+    Route::put('/shifts/{id}/update', [ShiftController::class, 'update'])->name('update');
 });
