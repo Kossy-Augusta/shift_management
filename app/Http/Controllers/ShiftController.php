c7ed7924214bbc22b5c262acc94e14766f6ca4bc
Kossy-Augusta <kossyudogu@gmail.com>
2024-06-14 16:31:11 +0100

    added update method and refactored the oher methods

--- app/Http/Controllers/ShiftController.php
+++ app/Http/Controllers/ShiftController.php
@@ -9,22 +9,34 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Auth\Events\Validated;
 use App\Http\Requests\CreateShiftRequest;
+use App\Http\Requests\UpdateShiftRequest;
+use App\Http\Resources\GetShiftsResource;
 
 class ShiftController extends Controller
 {
         //  Show all shifts
     public function index()
     {
-        // $products = DB::select('SELECT * FROM shift_staff ORDER BY created_at DESC LIMIT 21');
+        // $shift_staff = DB::select('SELECT * FROM shift_staff ORDER BY created_at DESC LIMIT 21');
         // get the last 20 inputs in descending oorder and paginate
-        $products = ShiftStaff::latest()->take(20)->paginate(5);
-         return response()->json(['error' => false, "data" => $products]);
+        try {
+            $shift_staff = ShiftStaff::orderBy('date', 'desc')
+                         ->orderBy('created_at', 'desc')
+                         ->take(20)
+                         ->paginate(5);
+
+            return response()->json(['error' => false, "data" => GetShiftsResource::collection($shift_staff)], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json(['error' => true, 'message' => $th->__toString()],Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
     // Create a shift schedule
     public function store(CreateShiftRequest $request)
     {
         $shifts = [];
-        foreach($request->emails as $item){
+        try {
+            foreach($request->emails as $item){
             $shifts[] = ShiftStaff::create([
                 'date' => $request->date,
                 'email' => $item,
@@ -32,7 +44,31 @@ class ShiftController extends Controller
             ]);
             
         }
+        } catch (\Throwable $th) {
+            return response()->json(['error' => true,'message' => $th->__toString()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        
 
         return response()->json(["error" => false, "data" => $shifts],Response::HTTP_CREATED);
+    }
+
+    public function update($id, UpdateShiftRequest $request)
+    {
+        
+        $shift = ShiftStaff::find($id);
+        if (!$shift){
+            return response()->json(["error" => true, "message" => "No shift found"], Response::HTTP_NO_CONTENT);
+        }
+        try {
+            $shift->update([
+            'date' => $request->date,
+            'shift_id' => $request->shift_id
+        ]);
+        } catch (\Throwable $th) {
+            return response()->json(['error' => true,'message' => $th->__toString()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        
+
+        return response()->json(['error' => false,'data' => $shift, "message" => "products updated sucessfully"], Response::HTTP_CREATED);
     }
 }
