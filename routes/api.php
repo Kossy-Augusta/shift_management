b73f658dd3d631d3094ea9fc3f5df5129d0d78a5
Kossy-Augusta <kossyudogu@gmail.com>
2024-06-26 09:46:34 +0100

    added an endoint to a protected route

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShiftController;
 use App\Http\Controllers\ShiftStaffController;
 
-// Public Route
-Route::get('/shiftstaff', [ShiftStaffController::class, 'index'])->name('all_shiftstaff');
 
 // Protected Routes
 Route::middleware([AuthApiKey::class])->group(function (){
+    Route::get('/shiftstaff', [ShiftStaffController::class, 'index'])->name('all_shiftstaff');
     Route::post('/shiftstaff/create', [ShiftStaffController::class, 'store'])->name('create');
     Route::put('/shiftstaff/{id}/update', [ShiftStaffController::class, 'update'])->name('update');
     Route::get('/shifts', [ShiftController::class, 'index'])->name('all_shifts');
